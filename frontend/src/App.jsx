cf1a2a0609f2f8fcb982690f5efbd7f45230b114
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-04-26 11:35:47 +0200

    tailwind configuration for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+  <h1 className=" flex flex-col justify-center items-center text-6xl mx-5 font-bold text-center underline text-blue-500">
+    Hello world!
+  </h1>
+</>
+
+  )
+}
+
+export default App
