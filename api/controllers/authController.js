a1859829ab7069dbb6d5818255391ecaafc32cc5
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-06-04 17:10:56 +0200

    login functionality

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -37,4 +37,28 @@ const userRegister = async (req,res)=>{
         expiresIn: '30d',
       });
 }
-module.exports = {userRegister}
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ message: 'Invalid credentials' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: 'Invalid credentials' });
+
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: '1d' }
+    );
+
+    res.json({ token, user: { name: user.name, email: user.email } });
+  } catch (err) {
+    res.status(500).json({ message: 'Login failed' });
+  }
+};
+module.exports = {userRegister,loginUser}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
 import { Routes,Route } from 'react-router-dom'
 import Login from './pages/Login'
-import Signup from './pages/Signup'
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Signup/>}/>
+      <Route path='/' element={<Login/>}/>
     </Routes>
 
   )
