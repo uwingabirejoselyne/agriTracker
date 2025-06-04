9e35685ec2281ce74af266e2f38cec8c9a054f8f
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-06-04 16:25:54 +0200

    saving farmer in db

--- api/server.js
+++ api/server.js
@@ -2,20 +2,26 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-
+const bodyParser = require('body-parser');
+const authRouter = require('./routes/authRoute');
 const connectDb = require('./config/mongodb');
-
 
 const app = express();
 
+
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+
+app.use('/api/auth', authRouter);
 
 app.get('/', (req, res) => {
   res.send('AgriTrackR API is running');
 });
-connectDb()
+
+
+connectDb();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// fWpNfVQFLoHdoS36
